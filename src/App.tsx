c011fb53a564d12b04909e43defc5c244ef06d58
sasidharakashyapchaturvedula9 <sasidharakashyapchaturvedula9@gmail.com>
2024-12-20 16:21:55 +0000

    Added

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+import{Button} from '@/components/ui/button';
+
+function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+          </SignInButton> 
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
+export default App
